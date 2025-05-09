f80a362322b43efae1dd22c9302e0cfd84e8ed58
kaiky <kaiky.santos.lima@escola.pr.gov.br>
2025-05-09 09:42:10 -0300

    Add files via upload

---
+++ exercicio/script.js
@@ -0,0 +1,14 @@
+function somar(){
+  let num1 =0;
+  let num2 =0;
+
+  
+  num1 = Number(window.prompt('digite um numero'))
+
+  num2 = Number(window.prompt('digite outro numero'))
+
+  let soma = num1 + num2
+
+  alert (soma)
+  
+} 
