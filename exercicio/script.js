f02fb9e6f551a342725c2a23fdbdc102e1a16aaa
kaiky <kaiky.santos.lima@escola.pr.gov.br>
2025-05-14 07:55:22 -0300

    Add files via upload

--- exercicio/script.js
+++ exercicio/script.js
@@ -1,14 +1,18 @@
-function somar(){
+function soma(){
   let num1 =0;
   let num2 =0;
 
   
-  num1 = Number(window.prompt('digite um numero'))
+  num1 = Number(window.prompt('digite um numero'));
+  num2 = Number(window.prompt('digite outro numero'));
 
-  num2 = Number(window.prompt('digite outro numero'))
+  let soma = num1 + num2;
 
-  let soma = num1 + num2
-
-  alert (soma)
+  alert (soma);
   
+  if(soma >10) {
+     alert("o resultado é maior que 10");
+  } else {
+    alert("Nao é maior que 10")
+  }
 } 
