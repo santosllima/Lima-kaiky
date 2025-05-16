e26bc1504a713569b92953918c3fe5b287438c17
kaiky <kaiky.santos.lima@escola.pr.gov.br>
2025-05-16 09:30:44 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,19 @@
+//calculadora simples com as 5 operações basicas:
+//Adição, Subtração, Multiplicação, Divisão e divisão sobre a sobra;
+let num1;
+let num2;
+let resultado
+function sobra() {
+   num1 = Number(window.prompt("digite o primeiro numero"))
+   num2 = Number(window.prompt("digite o segundo numero"))
+
+  resultado = num1 % num2
+  alert()
+}
+function Multiplicação() {
+   num1 = Number(window.prompt("digite o primeiro numero"))
+   num2 = Number(window.prompt("digite o segundo numero"))
+
+  resultado = num1 * num2
+  alert()
+}
